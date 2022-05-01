65bfe26b343e8611ecb66da65e86daa9250523a4
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-01 13:37:01 +0100

    created a simple setState example
    change the name by clicking a button

--- tudo-project/src/App.js
+++ tudo-project/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import Header from "./Header";
+import { useState } from "react";
 
 function App() {
-    const name = "Raeven";
+    const [name, setName] = useState("Guest");
+
+    function login() {
+        setName("Raeven");
+    }
 
     return (
         <div className="App">
             <Header title={name} />
             <p>Welcome to my blog</p>
+            <button onClick={login}>Log In</button>
         </div>
     );
 }
