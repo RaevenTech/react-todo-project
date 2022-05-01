8741321735322478960896410cb7b50065f4d057
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-01 14:14:50 +0100

    component lifecycle
    simple api fetch and display the data received

--- tudo-project/src/App.js
+++ tudo-project/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 import Header from "./Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Row } from "react-bootstrap";
 
 function App() {
     const [name, setName] = useState("Guest");
+    const [posts, setPosts] = useState([]);
+
+    useEffect(function () {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => response.json())
+            .then((json) => setPosts(json));
+    }, []);
 
     function login() {
         setName("Raeven");
@@ -14,6 +22,13 @@ function App() {
             <Header title={name} />
             <p>Welcome to my blog</p>
             <button onClick={login}>Log In</button>
+            <ul>
+                {posts.map((post) => (
+                    <Row key={post.id}>
+                        <li>{post.title}</li>
+                    </Row>
+                ))}
+            </ul>
         </div>
     );
 }
